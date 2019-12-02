d13a73ab774ccb26eddd5e649302daff7866a185
bdd <bdd@bdddeMacBook-Pro.local>
2019-12-02 09:26:40 +0800

    id222

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const tslib_1 = require("tslib");
+tslib_1.__exportStar(require("./exception"), exports);
+tslib_1.__exportStar(require("./cj-validator"), exports);
+tslib_1.__exportStar(require("./extended-validator"), exports);
+tslib_1.__exportStar(require("./util"), exports);
